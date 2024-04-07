678ef308db25ebea4d313af70ab6ad808cb3c179
Rasmus Pehrsson <pehrsson.rasmus@gmail.com>
2024-04-07 12:53:29 +0200

    Table for card

--- src/main/resources/schemas/createschema.sql
+++ src/main/resources/schemas/createschema.sql
@@ -29,6 +29,20 @@ CREATE TABLE IF NOT EXISTS Player (
   FOREIGN KEY (gameID) REFERENCES Game(gameID)
 );;
 
+CREATE TABLE IF NOT EXISTS Card (
+    gameID int NOT NULL,
+    playerID tinyint NOT NULL,
+    handPosition int NOT NULL,
+
+    cardType varchar(255),
+
+    PRIMARY KEY (gameID, playerID, handPosition),
+    FOREIGN KEY (gameID) REFERENCES Game(gameID),
+    FOREIGN KEY (playerID) REFERENCES Player(playerID)
+);;
+
+)
+
 SET FOREIGN_KEY_CHECKS = 1;;
 
 // TODO still some stuff missing here
