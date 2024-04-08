13f2442bd728b9acdb54e4c0f2a437c0c628c8bb
Rasmus Pehrsson <pehrsson.rasmus@gmail.com>
2024-04-08 15:47:22 +0200

    javadoc

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Rotator.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Rotator.java
@@ -7,18 +7,43 @@ import org.jetbrains.annotations.NotNull;
 
 import java.text.Format;
 
+/**
+ * A fieldaction that rotates the player standing on top
+ * @author s235436
+ */
 public class Rotator extends FieldAction {
 
+    /**
+     * An int value that determines if the rotation is counter clockwise (1) or clockwise(2)
+     * @author s235436
+     */
     private int rotation;
 
+    /**
+     * A method to find the rotation
+     * @return either 1 or 2 (CCW or CW)
+     * @author s235436
+     */
     public int getRotation(){
         return rotation;
     }
 
+    /**
+     * A method to set the rotation
+     * @param rotation the int value of either CCW or CW
+     * @author s235436
+     */
     public void setRotation(int rotation){
         this.rotation = rotation;
     }
 
+    /**
+     * Implementation of how the rotator works
+     * @param gameController the gameController of the respective game
+     * @param space the space this action should be executed for
+     * @return true of false depending on if completed or not
+     * @author s235436
+     */
     @Override
     public boolean doAction(@NotNull GameController gameController, @NotNull Space space){
         Player player = space.getPlayer();
