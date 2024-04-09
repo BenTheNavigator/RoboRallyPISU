5b5e4cee38d0c428ad30c6e8fc711732d61dd77d
Frederik Helge <frederikhelge@hotmail.com>
2024-04-09 07:48:54 +0200

    Trench was added, on the board and the function

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Trench.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Trench.java
@@ -4,6 +4,11 @@ import dk.dtu.compute.se.pisd.roborally.model.Player;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 import org.jetbrains.annotations.NotNull;
 /**
+ * This class represents the Action field trench that a robot can land on.
+ * The class Trench is a subclass of FieldAction.
+ * The class Trench contains a variable of the type space which is the space associated with the trench
+ * The doAction method contains the code that does the action when i robot falls into the trench
+ * Here it will take the player that shares a space with the trench and subtract 1 from the players checkpoint counter
  * @author s235458
  */
 public class Trench extends FieldAction{
