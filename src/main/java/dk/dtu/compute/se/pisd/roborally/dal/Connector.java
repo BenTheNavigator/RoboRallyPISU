2cee4a09797ae42a6d1bdbb356ef2d7abd9c2ada
BenTheNavigator <benjaminlgad@gmail.com>
2024-04-11 15:29:50 +0200

    disabled save to DB in appcontroller line 137, and erased my DB credentials in Connector

--- src/main/java/dk/dtu/compute/se/pisd/roborally/dal/Connector.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/dal/Connector.java
@@ -39,9 +39,9 @@ class Connector {
 	
     private static final String HOST     = "localhost";
     private static final int    PORT     = 3306;
-    private static final String DATABASE = "pisd";
+    private static final String DATABASE = "";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Gwz22evh";
+    private static final String PASSWORD = "";
 
     private static final String DELIMITER = ";;";
     
