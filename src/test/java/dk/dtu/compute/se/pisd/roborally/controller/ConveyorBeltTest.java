02200ccf4547469396e0e9102b59f3381d299ba3
Frederik Helge <frederikhelge@hotmail.com>
2024-04-25 15:19:49 +0200

    Convayorbelt test implemented

--- src/test/java/dk/dtu/compute/se/pisd/roborally/controller/ConveyorBeltTest.java
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/controller/ConveyorBeltTest.java
@@ -1,5 +1,26 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
 
-public class ConveyorBeltTest {
-    
+import dk.dtu.compute.se.pisd.roborally.fileaccess.LoadBoard;
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConveyorBeltTest {
+    @Test
+    void doAction() {
+        Board board = LoadBoard.loadBoard("roboloco");
+        GameController gameController = new GameController(board);
+        Player player = new Player(board, null, "TestPlayer");
+        board.addPlayer(player);
+        player.setSpace(board.getSpace(2, 0));
+        Space space = player.getSpace();
+        for (FieldAction action : space.getActions()) {
+            action.doAction(gameController, space);
+        }
+        Assertions.assertEquals(board.getSpace(3,0),player.getSpace(),"The player should move one space to the left");
+    }
 }
