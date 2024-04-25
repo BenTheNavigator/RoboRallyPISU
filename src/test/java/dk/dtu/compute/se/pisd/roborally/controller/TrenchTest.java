4a6f8975177f4e8315ca29037469b3f7840922c2
Frederik Helge <frederikhelge@hotmail.com>
2024-04-25 15:50:57 +0200

    Trench test is implemented aswell as author tags and some comments

--- src/test/java/dk/dtu/compute/se/pisd/roborally/controller/TrenchTest.java
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/controller/TrenchTest.java
@@ -1,5 +1,6 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
 
+import com.mysql.cj.protocol.x.XMessage;
 import dk.dtu.compute.se.pisd.roborally.fileaccess.LoadBoard;
 import dk.dtu.compute.se.pisd.roborally.model.Board;
 import dk.dtu.compute.se.pisd.roborally.model.Player;
@@ -10,20 +11,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 /**
  * @author s235458
- * This test checks, that our conveyerbelt will move one player, when the player lands on the conveyerbelt.
+ * This test class is created to test the Trench, which it does in the doAction method.
+ * It verifies if the player landing in the Trench, loses a checkpoint, after landing in the Trench.
+ * It Checks that a players checkpoint counter has gone down by 1.
  */
-class ConveyorBeltTest {
+class TrenchTest {
     @Test
     void doAction() {
-        Board board = LoadBoard.loadBoard("roboloco");
+        Board board = LoadBoard.loadBoard("poseidon");
         GameController gameController = new GameController(board);
         Player player = new Player(board, null, "TestPlayer");
         board.addPlayer(player);
-        player.setSpace(board.getSpace(2, 0));
+        player.setSpace(board.getSpace(1, 0));
+        player.setCheckpointCounter((1));
         Space space = player.getSpace();
         for (FieldAction action : space.getActions()) {
             action.doAction(gameController, space);
         }
-        Assertions.assertEquals(board.getSpace(3,0),player.getSpace(),"The player should move one space to the left");
+
+        Assertions.assertEquals(player.getCheckpointCounter(),0,"The player has lost one checkpoint");
+        Assertions.assertEquals(board.getSpace(1,0),player.getSpace(),"The player should still be on the same space");
     }
 }
